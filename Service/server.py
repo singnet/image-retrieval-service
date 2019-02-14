4b990d04acc5ae04143f7d42beb2aa40b2c51d87
Tesfa Yohannes <tmyohannes@gmail.com>
2019-02-14 11:02:26 +0300

    Remove the remnants of jsonrpc error handling

--- Service/server.py
+++ Service/server.py
@@ -11,9 +11,12 @@ import similarImage
 class SimilarImageServicer(image_retrival_pb2_grpc.SimilarImageServicer):
     def FindSimilar(self, request, context):
         if request.image is None:
-            raise InvalidParams("Image is required")
+            context.set_code(grpc.StatusCode.INVALID_ARGUMENT)
+            context.set_details("Image is required")
+            return image_retrival_pb2.ImageFileOut()
         if not request.similarity == "Test" or not request.similarity == "CosineDistance" or request.similarity == "EuclideanDistance":
-            raise InvalidParams("Similarity Measure has to be one of: Test, CosineDistance, EuclideanDistance")
+            context.set_code(grpc.StatusCode.INVALID_ARGUMENT)
+            context.set_details("Similarity Measure has to be one of: Test, CosineDistance, EuclideanDistance")
         response = image_retrival_pb2.ImageFileOut()
         response.imageOut1, response.imageOut2, response.imageOut3, response.imageOut4, response.imageOut5 = similarImage.find_similar(
             input_image=request.image, img_similarity=request.similarity)
--- deploy_service.sh
+++ deploy_service.sh
@@ -9,9 +9,9 @@ python generate_hashtable.py
 
 if [ ! -x "$(command -v nvidia-docker)" ]; then
     echo "Building CPU based Image"
-    docker build --file Dockerfile . -t singularitnet:image-retrieval-cpu
+    docker build --file Dockerfile . -t singularitynet:image-retrieval-cpu
 else
     echo "Building GPU based Image"
-    docker build --file Dockerfile.gpu . -t singularitnet:image-retrieval-gpu
+    docker build --file Dockerfile.gpu . -t singularitynet:image-retrieval-gpu
 fi
 
