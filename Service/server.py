5ca2625beb9caa9b3fd9fc4237d076f645c66c77
Tesfa Yohannes <tmyohannes@gmail.com>
2019-02-14 10:57:51 +0300

    Remove remnants of jsonropc error handling

--- Service/server.py
+++ Service/server.py
@@ -11,9 +11,13 @@ import similarImage
 class SimilarImageServicer(image_retrival_pb2_grpc.SimilarImageServicer):
     def FindSimilar(self, request, context):
         if request.image is None:
-            raise InvalidParams("Image is required")
-        if not reqest.similarity == "Test" or not request.similarity == "CosineDistance" or request.similarity == "EuclideanDistance":
-            raise InvalidParams("Similarity Measure has to be one of: Test, CosineDistance, EuclideanDistance")
+            context.set_code(grpc.StatusCode.INVALID_ARGUMENT)
+            context.set_details("Image is required")
+            return image_retrival_pb2.ImageFileOut()
+        if not request.similarity in ["Test", "CosineDistance", "EuclideanDistance"]:
+            context.set_code(grpc.StatusCode.INVALID_ARGUMENT)
+            context.set_details("Similarity Measure has to be one of: Test, CosineDistance, EuclideanDistance")
+            return image_retrival_pb2.ImageFileOut()
         response = image_retrival_pb2.ImageFileOut()
         response.imageOut1, response.imageOut2, response.imageOut3, response.imageOut4, response.imageOut5 = similarImage.find_similar(
             input_image=request.image, img_similarity=request.similarity)
@@ -34,6 +38,7 @@ class Server():
 
     def stop_server(self):
         self.server.stop(0)
+
 
 if __name__ == '__main__':
     server = Server()
