22de0609347e4b0fcecc494d57e890370a637142
Tesfa Yohannes <tmyohannes@gmail.com>
2019-02-28 11:46:28 +0300

    Map port for mainnet service

--- run_docker.sh
+++ run_docker.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 sudo docker build --file Dockerfile . -t singularitynet:image-retrieval-cpu
-sudo docker run -it -v /etc/letsencrypt:/etc/letsencrypt -v /home/zelalem/Image-retrieval-in-pytorch/data/classed_data:/image-retrieval-in-pytorch/data/classed_data --name image-retrieval-service -d -p 8003:8003 -p 8004:8004 singularitynet:image-retrieval-cpu
+sudo docker run -it -v /home/zelalem/Image-retrieval-in-pytorch/etcd:/image-retrieval-in-pytorch/etcd -v /etc/letsencrypt:/etc/letsencrypt -v /home/zelalem/Image-retrieval-in-pytorch/data/classed_data:/image-retrieval-in-pytorch/data/classed_data --name image-retrieval-service -d -p 8014:8014 -p 8004:8004 singularitynet:image-retrieval-cpu
